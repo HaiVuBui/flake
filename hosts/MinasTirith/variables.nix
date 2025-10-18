6614515215d795a8932963e03f42d09c32bdf401
HaiVuBui <buivuhai1105@gmail.com>
2025-10-19 03:17:54 +0700

    (feat) use frieren wallpaper for color scheme

--- hosts/MinasTirith/variables.nix
+++ hosts/MinasTirith/variables.nix
@@ -72,7 +72,7 @@
   # # This will set your color palette
   # # Default background
   # # Add new images to ~/zaneyos/wallpapers
-  # stylixImage = ../../wallpapers/mountainscapedark.jpg;
+  stylixImage = ../../wallpapers/frieren.jpg;
   # #stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
   # #stylixImage = ../../wallpapers/Anime-Purple-eyes.png;
   # #stylixImage = ../../wallpapers/Rainnight.jpg;
