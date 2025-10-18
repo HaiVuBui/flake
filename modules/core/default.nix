4978791f051472edfd5190e792194fb4fb8f7b28
HaiVuBui <buivuhai1105@gmail.com>
2025-10-18 23:39:16 +0700

    (feat) add core.security module

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -21,7 +21,7 @@ in
     # ./printing.nix
     # Conditionally import the display manager module
     # (if vars.displayManager == "tui" then ./greetd.nix else ./sddm.nix)
-    # ./security.nix
+    ./security.nix
     ./services.nix
     # ./steam.nix
     # ./stylix.nix
